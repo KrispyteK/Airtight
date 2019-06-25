13a7c8248195ed9c7160a862f278570552563fd6
Blieper <yoranmandema@eml.cc>
2019-06-26 00:03:59 +0200

    interaction system

---
+++ Assets/Scripts/Interactables/CubeSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+    public GameObject cube;
+    public float timeBetweenUse = 2f;
+    public GameObject instance;
+
+    private float useTime = 0f;
+
+
+    public void SpawnCube () {
+        if ((Time.timeSinceLevelLoad - useTime) > timeBetweenUse) {
+            Destroy(instance);
+
+            useTime = Time.timeSinceLevelLoad;
+
+            instance = Instantiate(cube, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    public float distance = 1.5f;
+    public LayerMask layerMask;
+
+    void Update() {
+        if (Input.GetButtonDown("Interact")) {
+            var rayCast = Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, distance, layerMask);
+
+            if (rayCast) {
+                var interactable = hit.collider.GetComponent<Interactable>();
+
+                if (interactable) {
+                    interactable.onInteract.Invoke();
+                }
+            }
+        }
+    }
+}
