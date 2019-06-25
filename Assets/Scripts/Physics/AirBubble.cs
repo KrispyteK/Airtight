73eb217b0a814572c58116f07ca0af7f95cb8e7d
Blieper <yoranmandema@eml.cc>
2019-06-26 00:03:39 +0200

    air bubble effects + size improvements

--- Assets/Scripts/Physics/AirBubble.cs
+++ Assets/Scripts/Physics/AirBubble.cs
@@ -2,36 +2,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.VFX;
 
 public class AirBubble : MonoBehaviour {
 
+    public AnimationCurve sizeCurve;
     public float maxSize = 2f;
     public float size = 0f;
     public float growTime = 0.5f;
+    public float sizeChangeSmoothing = 10f;
     public float velocity = 4f;
     public float force = 10f;
 
     public LayerMask layerMask;
     public LayerMask overlapMask;
 
+    public VisualEffect visualEffect;
+
     private SphereCollider sphereCollider;
     private List<Collider> colliders = new List<Collider>();
+    private float t;
+    private float smoothSize;
 
     void Start() {
         sphereCollider = GetComponent<SphereCollider>();
     }
 
     void Update() {
-        var rayCast = Physics.SphereCast(transform.position, size, transform.forward, out RaycastHit hit, velocity * Time.deltaTime, layerMask);
-        var overlap = Physics.CheckSphere(transform.position, Mathf.Clamp(size + Time.deltaTime / growTime, 0,maxSize), overlapMask);
+        t = Mathf.Min(t + Time.deltaTime, sizeCurve.keys[sizeCurve.length - 1].time);
 
-        size = Mathf.Clamp(size + Time.deltaTime / growTime * (overlap ? -1 : 1), 0, maxSize);
+        var _maxSize = maxSize * sizeCurve.Evaluate(t);
 
-        sphereCollider.radius = size;
+        var rayCast = Physics.SphereCast(transform.position, 0.25f, transform.forward, out RaycastHit hit, velocity * Time.deltaTime, layerMask);
+        var overlap = Physics.CheckSphere(transform.position, Mathf.Clamp(size + Time.deltaTime / growTime, 0, _maxSize), overlapMask);
+
+        size = Mathf.Clamp(size + Time.deltaTime / growTime * (overlap ? -1 : 1), 0, _maxSize);
+
+        smoothSize = Mathf.Lerp(size,smoothSize, sizeChangeSmoothing * Time.deltaTime);
+
+        visualEffect.SetFloat("Radius", smoothSize);
+
+        transform.localScale = Vector3.one * smoothSize * 2;
 
         transform.position = transform.position + transform.forward * velocity * Time.deltaTime;
 
-        if (rayCast && Vector3.Dot(-hit.normal, transform.forward) > 0.8f) {
+        if (rayCast) {
             Destroy(gameObject);
         }
     }
