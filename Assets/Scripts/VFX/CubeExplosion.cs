cfa1442a5446c6f13f649dd1daf4d18b3f76e768
Blieper <yoranmandema@eml.cc>
2019-06-30 00:05:34 +0200

    cube explosion vfx

---
+++ Assets/Scripts/VFX/CubeExplosion.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeExplosion : MonoBehaviour {
+    public GameObject effect;
+
+    void OnDestroy () {
+        Instantiate(effect, transform.position, transform.rotation);
+    }
+}
