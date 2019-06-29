46a6f7f90eef5b18ead45fb83c6db9ee2ac2a246
Blieper <yoranmandema@eml.cc>
2019-06-29 23:18:08 +0200

    button visuals

--- Assets/Scripts/Interactables/Buttons/Button.cs
+++ Assets/Scripts/Interactables/Buttons/Button.cs
@@ -3,5 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Button : Interactable {
-    
+    public bool isPressed;
+
+    private void Start () {
+        onStartInteract.AddListener(() => {
+            if (!isActive) return;
+
+            isPressed = true;
+        });
+
+        onEndInteract.AddListener(() => {
+            if (!isActive) return;
+
+            isPressed = false;
+        });
+    }
 }
