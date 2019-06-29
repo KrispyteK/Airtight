54f8fbdc7d4e1401d016460932c26b2a55bf3f46
Blieper <yoranmandema@eml.cc>
2019-06-29 23:17:51 +0200

    Timer behaviour

---
+++ Assets/Scripts/Functional/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour {
+
+    public UnityEvent onFinish;
+
+    public void StartTimer (float seconds) {
+        StartCoroutine(TimerRoutine(seconds));
+    }
+
+    private IEnumerator TimerRoutine (float seconds) {
+        yield return new WaitForSeconds(seconds);
+
+        onFinish.Invoke();
+    }
+}
