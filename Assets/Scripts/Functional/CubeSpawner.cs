fb06acf19e82bffb48da1b1db38928104a46c7d2
Blieper <yoranmandema@eml.cc>
2019-06-29 23:18:48 +0200

    reorganise interactable and functional prefabs

---
+++ Assets/Scripts/Functional/CubeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+    public GameObject cube;
+    public bool canSpawn = true;
+    public GameObject instance;
+
+    public void SetActive (bool active) {
+        canSpawn = active;
+    }
+
+    public void SpawnCube () {
+        if (canSpawn) {
+            if (instance) Destroy(instance);
+
+            instance = Instantiate(cube, transform.position, transform.rotation);
+        }
+    }
+}
--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public abstract class Interactable : MonoBehaviour {
+    public bool isActive = true;
+
+    public void SetActive (bool active) {
+        isActive = active;
+    }
+
     public UnityEvent onInteract;
     public UnityEvent onStartInteract;
     public UnityEvent onEndInteract;
