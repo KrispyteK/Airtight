3bb767fc3b6202a7e376ba62054c5913e42b8fe8
Blieper <yoranmandema@eml.cc>
2019-06-30 20:47:08 +0200

    console max line limit

--- Assets/Scripts/Console/ConsoleText.cs
+++ Assets/Scripts/Console/ConsoleText.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
+using System.Linq;
 
 public class ConsoleText : MonoBehaviour {
 
     private TMPro.TextMeshProUGUI textMesh;
-
     private StringBuilder stringBuilder;
+    public int lines;
 
     void Start() {
         textMesh = GetComponent<TMPro.TextMeshProUGUI>();
@@ -16,13 +17,22 @@ public class ConsoleText : MonoBehaviour {
     public void AddMessage (string message) {
         if (stringBuilder == null) stringBuilder = new StringBuilder();
 
-        stringBuilder.Append(message + '\n');
+        string str = System.Convert.ToString(stringBuilder);
+        int numLines = str.Split('\n').Length;
+
+        if (numLines > 250) {
+            var firstLine = stringBuilder.ToString().IndexOf(System.Environment.NewLine, System.StringComparison.Ordinal);
+
+            stringBuilder.Remove(0, firstLine + System.Environment.NewLine.Length);
+        }
+
+        stringBuilder.Append(message + System.Environment.NewLine);
     }
 
     public void UpdateText () {
         if (!textMesh) textMesh = GetComponent<TMPro.TextMeshProUGUI>();
 
-        textMesh.text = stringBuilder.ToString();
+        textMesh.text = stringBuilder?.ToString();
         textMesh.autoSizeTextContainer = true;
     }
 
